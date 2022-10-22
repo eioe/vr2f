146c4961bb3ecb3ca30d24fd9b6734eaed68cef6
Felix Klotzsche <klotzsche@cbs.mpg.de>
2022-10-22 18:24:40 +0200

    add staticinfo

---
+++ src/Python/vr2fem_analyses/staticinfo.py
@@ -0,0 +1,35 @@
+"""Store global variables
+
+Keep paths, colors, etc. to access them throughout the project.
+"""
+import os
+import sys
+from pathlib import Path
+
+MAIN_PATH = ''
+
+class VR2FEMPATHS:
+    """Store relevant paths for the project.
+    """
+    def __init__(self) -> None:
+        self.PATH_PROJECT = Path(__file__).parents[4]
+        if self.PATH_PROJECT.parts[-1] != 'vrstereofem':
+            raise ValueError('staticinfo seems to be in the wrong folder. \
+                              Cannot point to project folder.')
+        self.DATA_SUBJECTS = Path(self.PATH_PROJECT, 'Data', 'Subjects')
+        self.DATA_RAWFIF = Path(self.PATH_PROJECT, 'Data', 'raw_fif')
+    def getPath(self, key: str) -> Path:
+        """Get path.
+
+        Parameters
+        ----------
+        key : str
+            Identifier for the path.
+
+        Returns
+        -------
+        Path
+            Path object.
+        """
+        return getattr(self, key)
+
