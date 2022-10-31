5c92efb2d995d6379a0f8b2682fdce657005aeeb
Felix Klotzsche <klotzsche@cbs.mpg.de>
2022-10-31 18:59:49 +0100

    fix path

--- src/Python/vr2fem_analyses/staticinfo.py
+++ src/Python/vr2fem_analyses/staticinfo.py
@@ -4,25 +4,27 @@ Keep paths, colors, etc. to access them throughout the project.
 """
 from pathlib import Path
 
-MAIN_PATH = ''
+MAIN_PATH = ""
 
 
 class PATHS:
-    """Store relevant paths for the project.
-    """
+    """Store relevant paths for the project."""
+
     def __init__(self) -> None:
         self.PATH_PROJECT = Path(__file__).parents[4]
-        if self.PATH_PROJECT.parts[-1] != 'vrstereofem':
-            raise ValueError('staticinfo seems to be in the wrong folder. \
-                              Cannot point to project folder.')
-        self.DATA_SUBJECTS = Path(self.PATH_PROJECT, 'Data', 'Subjects')
-        self.DATA_00_RAWFIF = Path(self.PATH_PROJECT, 'Data', 'raw_fif')
-        self.DATA_01_EPO = Path(self.PATH_PROJECT, 'Data', '01_epo')
-        self.DATA_02_ICA_AR = Path(self.PATH_PROJECT, 'Data', '02_ica', 'ar')
-        self.DATA_02_ICA = Path(self.PATH_PROJECT, 'Data', '02_ica')
-        self.DATA_02_EOGANNOT = Path(self.PATH_PROJECT, 'Data', '02_eogannot')
-        self.DATA_02_POSTICA = Path(self.PATH_PROJECT, 'Data', '02_ica', 'cleaneddata'),
-        self.DATA_03_AR = Path(self.PATH_PROJECT, 'Data', '03_ar')
+        if self.PATH_PROJECT.parts[-1] != "vrstereofem":
+            raise ValueError(
+                "staticinfo seems to be in the wrong folder. \
+                              Cannot point to project folder."
+            )
+        self.DATA_SUBJECTS = Path(self.PATH_PROJECT, "Data", "Subjects")
+        self.DATA_00_RAWFIF = Path(self.PATH_PROJECT, "Data", "raw_fif")
+        self.DATA_01_EPO = Path(self.PATH_PROJECT, "Data", "01_epo")
+        self.DATA_02_ICA_AR = Path(self.PATH_PROJECT, "Data", "02_ica", "ar")
+        self.DATA_02_ICA = Path(self.PATH_PROJECT, "Data", "02_ica")
+        self.DATA_02_EOGANNOT = Path(self.PATH_PROJECT, "Data", "02_eogannot")
+        self.DATA_02_POSTICA = Path(self.PATH_PROJECT, "Data", "02_ica", "cleaneddata")
+        self.DATA_03_AR = Path(self.PATH_PROJECT, "Data", "03_ar")
 
     def getPath(self, key: str) -> Path:
         """Get path.
